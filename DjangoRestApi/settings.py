421d5e2771d2f226cf41bee709800358a65a941d
Tobias Pilarche <tobsailbot@gmail.com>
2022-07-25 23:16:33 -0300

    Update settings.py

--- DjangoRestApi/settings.py
+++ DjangoRestApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7reb9d$g$mhgw0_)@y%+jh=$bnj9f*)v4zz1b4b-p+^^=zd8(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dental-soft.herokuapp.com']
 
